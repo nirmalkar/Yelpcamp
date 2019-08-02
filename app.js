41285c50faf57233a77ce73e03887e4fdf0baa94
hemantkumar2 <hemantnirmlkar17@gmail.com>
2019-08-02 10:15:32 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine","ejs");
+
+app.get("/",function(req, res){
+    res.render("landing"); 
+});
+app.get("/campgrounds", function(req, res){
+var campgrounds = [
+{name:"Rishikesh",image:"https://pixabay.com/get/57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c732b7cd5964dc25d_340.jpg"},
+{name:"Spiti Valley",image:"https://pixabay.com/get/57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c732b7cd5964dc25d_340.jpg"},
+{name:"Jaisalmer",image:"https://pixabay.com/get/57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c732b7cd5964dc25d_340.jpg"},
+{name:"Mussoorie",image:"https://pixabay.com/get/57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c732b7cd5964dc25d_340.jpg"}
+];
+res.render("campgrounds",{campgrounds:campgrounds});
+});
+
+
+app.listen(3000, function(){
+    console.log("The Yelpcmap server has started");
+});
