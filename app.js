bfb9400e04eb3eae0b1de823a4e3c4ed2acb5a0d
hemantkumar2 <hemantnirmlkar17@gmail.com>
2019-08-07 02:48:47 +0530

    Linked mongo DB

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 
-var campgrounds = [
-    
-    { name: "Jaisalmer", image: "https://images.unsplash.com/photo-1546811740-23e671faf31c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" },
-    { name: "Mussoorie", image: "https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" },
-    { name: "Rishikesh", image: "https://images.unsplash.com/photo-1506535995048-638aa1b62b77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" },
-    { name: "Spiti Valley", image: "https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" },
-    { name: "Yosemite National Park", image: "https://images.unsplash.com/photo-1546811740-23e671faf31c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" },
-    { name: "Miyajima", image: "https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" },
-    { name: "Boya Lake Provincial Park", image: "https://images.unsplash.com/photo-1506535995048-638aa1b62b77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" },
-    { name: "Big Sur", image: "https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" },
-    { name: "The Isle of Arran", image: "https://images.unsplash.com/photo-1546811740-23e671faf31c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" },
-    { name: "Flamenco Beach in Culebra", image: "https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" },
-    { name: "Yellowstone National Park", image: "https://images.unsplash.com/photo-1506535995048-638aa1b62b77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" },
-    { name: "Lake District National Park", image: "https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" },
-    { name: "Lake Myvatn", image: "https://images.unsplash.com/photo-1546811740-23e671faf31c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" },
-    { name: "Jasper National Park", image: "https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" },
-    { name: "Sardinia", image: "https://images.unsplash.com/photo-1506535995048-638aa1b62b77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" },
-    { name: "Ladakh", image: "https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" }
-];
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name:String,
+    image:String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campgrounds.create({ name: "Mussoorie", image: "https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" }, function(err, campground){
+//  if(err){
+//      console.log("Something wrong, Err!!");
+//      console.log(err);
+//  } else{
+//      console.log("Newly created Campground");
+//      console.log(campground);
+//  }   
+// });
+
+
 
 app.get("/",function(req, res){
     res.render("landing"); 
 });
 app.get("/campgrounds", function(req, res){
+    //get all campgrounds from db
+    Campground.find({},function(err,allCampgrounds){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("campgrounds", { campgrounds: allCampgrounds });
+        }
+    });
 
-res.render("campgrounds",{campgrounds:campgrounds});
+
 });
 
 app.post("/campgrounds",function(req , res){
@@ -38,9 +48,16 @@ app.post("/campgrounds",function(req , res){
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name:name, image:image} 
-    campgrounds.push(newCampground);
-    //redirect back to the campground page
-    res.redirect("/campgrounds");
+    //create a newcampground and save it to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/campgrounds");
+        }
+    });
+    //redirect back to the campground page 
+    
 });
 
 app.get("/campgrounds/new", function(req, res){
